272c94706eb80e50acec3446c54c7639b02489b0
nkagan <nkagan@dar.io>
2022-07-06 12:56:46 +0600

    task-5

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,67 +1,89 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
+import { url } from 'inspector';
 
 const useStyles = makeStyles((theme) => ({
-  content: {
-    padding: theme.spacing(3, 0, 3),
-  },
+    content: {
+        minWidth: 255,
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(3, 0, 3),
+    },
 }));
 
 type CSVFileImportProps = {
-  url: string,
-  title: string
+    title: string;
 };
 
-export default function CSVFileImport({url, title}: CSVFileImportProps) {
-  const classes = useStyles();
-  const [file, setFile] = useState<any>();
+export default function CSVFileImport({ title }: CSVFileImportProps) {
+    const classes = useStyles();
+    const [file, setFile] = useState<any>();
+    const [uploadUrl, setUploadUrl] = useState<any>();
+    const [fileName, setFileName] = useState('');
 
-  const onFileChange = (e: any) => {
-    console.log(e);
-    let files = e.target.files || e.dataTransfer.files
-    if (!files.length) return
-    setFile(files.item(0));
-  };
+    const createFile = (file: any) => {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+            console.log(e.target.result);
+            setFileName(file.name);
+            setFile(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
 
-  const removeFile = () => {
-    setFile('');
-  };
+    const onFileChange = (e: any) => {
+        let files = e.target.files || e.dataTransfer.files;
+        if (!files.length) return;
+        createFile(files[0]);
+    };
 
-  const uploadFile = async (e: any) => {
-      // Get the presigned URL
-      const response = await axios({
-        method: 'GET',
-        url,
-        params: {
-          name: encodeURIComponent(file.name)
+    const removeFile = () => {
+        setFile('');
+    };
+
+    const uploadFile = async (e: any) => {
+        // Get the presigned URL
+        const response = await axios({
+            method: 'GET',
+            url: `https://5j0uel8y5i.execute-api.us-east-1.amazonaws.com/dev/import/${fileName}`,
+        });
+        console.log('Response: ', response.data);
+        console.log('Uploading: ', file);
+        let binary = atob(file.split(',')[1]);
+        let array = [];
+        for (var i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
         }
-      })
-      console.log('File to upload: ', file.name)
-      console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
-        method: 'PUT',
-        body: file
-      })
-      console.log('Result: ', result)
-      setFile('');
-    }
-  ;
-
-  return (
-    <div className={classes.content}>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      {!file ? (
-          <input type="file" onChange={onFileChange}/>
-      ) : (
-        <div>
-          <button onClick={removeFile}>Remove file</button>
-          <button onClick={uploadFile}>Upload file</button>
+        let blobData = new Blob([new Uint8Array(array)], { type: 'text/plain' });
+        console.log('Uploading to: ', response.data.url);
+        const result = await fetch(response.data.url, {
+            method: 'PUT',
+            body: blobData,
+        });
+        console.log('Result: ', result);
+        // Final URL for the user doesn't need the query string params
+        setUploadUrl('');
+        setFile('');
+    };
+    return (
+        <div className={classes.content}>
+            <Typography variant="h6" gutterBottom>
+                {title}
+            </Typography>
+            {!file ? (
+                <input type="file" onChange={onFileChange} />
+            ) : (
+                <div>
+                    {!uploadUrl && <button onClick={removeFile}>Remove file</button>}
+                    {!uploadUrl && <button onClick={uploadFile}>Upload file</button>}
+                </div>
+            )}
+            {fileName && (
+                <Typography variant="subtitle1" gutterBottom>
+                    File name: {fileName}
+                </Typography>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
